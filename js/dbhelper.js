9ac06a3c806a39b738632bd9f04d8da4d0397d0c
DrewDen <denson10@gmail.com>
2019-01-05 19:20:33 -0500

    fixed structure

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,7 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 5500 
+    const port = 8000
     return `http://localhost:${port}/data/restaurants.json`;
   }
 
